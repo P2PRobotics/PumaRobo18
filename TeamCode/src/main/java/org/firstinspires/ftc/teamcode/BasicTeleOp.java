9d8a84fce05c3e1853ea90335e374416b12068c8
VVestin <wmmetzler01@gmail.com>
2018-09-29 11:50:21 -0600

    setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -23,15 +23,17 @@ public class BasicTeleOp extends OpMode {
 
     @Override
     public void init(){
-        //initialize motors, retrieve configs from RevHub and Controllers
-        leftMotor1 = hardwareMap.dcMotor.get("l1");
+        //initializes motors, retrieve configs from RevHub and Controllers
+        leftMotor1 = hardwareMap.dcMotor.get("l1"); //front wheels (dirMotor1) go backwards
         leftMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
         //hey software-- don't forget to configure motors w these exact names!!
        leftMotor2 = hardwareMap.dcMotor.get("l2");
         leftMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         rightMotor1 = hardwareMap.dcMotor.get("r1");
         rightMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
 
         rightMotor2 = hardwareMap.dcMotor.get("r2");
         rightMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -48,7 +50,7 @@ public class BasicTeleOp extends OpMode {
         leftMotor2.setPower(0);
         rightMotor1.setPower(0);
         rightMotor2.setPower(0);
-        //pivoting/turning
+        //pivoting and turning, uses left and right triggers
         if (gamepad1.left_trigger > 0.05) {
             double trigger = gamepad1.left_trigger;
             leftMotor1.setPower(-Math.pow(trigger, 5));
@@ -65,9 +67,19 @@ public class BasicTeleOp extends OpMode {
             rightMotor2.setPower(-Math.pow(trigger, 5));
             return;
         }
+        double rawx = gamepad1.right_stick_x;
+        double rawy = -gamepad1.right_stick_y;
+        double x= Math.pow(rawx, 7); //adjust sensitivity?
+        double y = Math.pow(rawy, 7);
+        if (x != 0 || y != 0) {
+            double n = ((x + y) / Math.sqrt(2.0)); // n is the power of the motors in the +x +y direction
+            double m = ((x - y) / Math.sqrt(2.0)); // m is the power of the motors in the +x -y direction
 
-        //relative turning!!
-        //triggers for turning/pivoting
+            leftMotor1.setPower(m);
+            leftMotor2.setPower(m);
+            rightMotor1.setPower(n);
+            rightMotor2.setPower(n);
+        }
         //LB lowers extender, RB raises extender
         //A opens grabber, X closes
         //Right Joystick for movement
