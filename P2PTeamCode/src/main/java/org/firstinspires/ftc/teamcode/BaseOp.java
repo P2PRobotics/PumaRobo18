68b2d392e6fd04f926f36c064ba83849b122b9d4
pumarobotics <p2probotics@gmail.com>
2018-12-08 11:10:24 -0700

    created curveDrive method in BaseOp, needs testing

--- P2PTeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOp.java
+++ P2PTeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOp.java
@@ -44,16 +44,16 @@ public class BaseOp extends OpMode {
 
         //make sure these match what's in the config
 
-        liftMotor= hardwareMap.dcMotor.get("liftmo"); //revHub 2, motor port 0
+        liftMotor = hardwareMap.dcMotor.get("liftmo"); //revHub 2, motor port 0
         liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         latchBarM = hardwareMap.servo.get("latchBarM"); //revHub 1, servo port 0
         latchCupM = hardwareMap.servo.get("latchCupM"); //revHub 1, servo port 1
 
-        intake1=hardwareMap.dcMotor.get("intake1"); //revHub 2, motor port 1
+        intake1 = hardwareMap.dcMotor.get("intake1"); //revHub 2, motor port 1
         intake1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        intake2=hardwareMap.crservo.get("intake2"); //revHub 1, servo port 2
+        intake2 = hardwareMap.crservo.get("intake2"); //revHub 1, servo port 2
 
 
         lmotor1.setPower(0);
@@ -64,42 +64,48 @@ public class BaseOp extends OpMode {
         liftMotor.setPower(0);
         intake1.setPower(0);
     }
-    public void start() {}
-    public void loop() {}
 
-    public void wheelIn(){
-            intake1.setPower(0.75);
+    public void start() {
+    }
+
+    public void loop() {
+    }
+
+    public void wheelIn() {
+        intake1.setPower(0.75);
     }
 
     public void wheelOut() {
-            intake1.setPower(-0.75);
+        intake1.setPower(-0.75);
     }
-    public void wheelStop(){
+
+    public void wheelStop() {
         intake1.setPower(0);
     }
 
-    public void raiseContainer(boolean raised){
-        if(raised) {
-           intake2.setPower(0.5);
+    public void raiseContainer(boolean raised) {
+        if (raised) {
+            intake2.setPower(0.5);
         } else {
-           intake2.setPower(-0.5);
+            intake2.setPower(-0.5);
         }
     }
 
 
-    public void lift(double speed){
+    public void lift(double speed) {
         liftMotor.setPower(speed);
     }
 
     public void latchBar(boolean latchedBar) {
-        if(latchedBar) {
+        if (latchedBar) {
             latchBarM.setPosition(0.5);
         } else {
             latchBarM.setPosition(0);
         }
     }
-    public void latchCup(boolean latchedCup){
-        if(latchedCup){
+
+    public void latchCup(boolean latchedCup) {
+        if (latchedCup) {
             latchCupM.setPosition(0.5);
         } else {
             latchCupM.setPosition(0);
@@ -107,18 +113,17 @@ public class BaseOp extends OpMode {
     }
 
 
-
-    public void turn(double speed){
+    public void turn(double speed) {
         double v = adjustedSpeed(speed);
         double curPowRm1 = rmotor1.getPower();
-        if ((Math.abs(curPowRm1)+deltamax) > 1.0)
-        rmotor1.setPower(v);
+        if ((Math.abs(curPowRm1) + deltamax) > 1.0)
+            rmotor1.setPower(v);
         rmotor2.setPower(v);
         lmotor1.setPower(v);
         lmotor2.setPower(v);
     }
 
-    public void move(double speed){
+    public void move(double speed) {
         double v = adjustedSpeed(speed);
         lmotor1.setPower(-v);
         lmotor2.setPower(-v);
@@ -135,8 +140,36 @@ public class BaseOp extends OpMode {
         else return speed;
         //Adjust speed so it doesn't slow down too much!!!!!!!
     }
-    public void curveDrive(double speed, double magnitude){
 
+    public void curveDrive(double curve, double magnitude) {
+        double leftOutput;
+        double rightOutput;
+        double sensitivity = 0; //THIS IS A PLACEHOLDER. FIND A GOOD NUMBER FOR SENSITVITY
+        if (curve < 0) {
+            double value = Math.log(-curve);
+            double ratio = (value - sensitivity) / (value + sensitivity);
+            if (ratio == 0) {
+                ratio = .0000000001;
+            }
+            leftOutput = magnitude / ratio;
+            rightOutput = magnitude;
+        } else if (curve > 0) {
+            double value = Math.log(curve);
+            double ratio = (value - sensitivity) / (value + sensitivity);
+            if (ratio == 0) {
+                ratio = .0000000001;
+            }
+            leftOutput = magnitude;
+            rightOutput = magnitude / ratio;
+        } else {
+            leftOutput = magnitude;
+            rightOutput = magnitude;
+        }
+        lmotor1.setPower(-leftOutput);
+        lmotor2.setPower(-leftOutput);
+        rmotor1.setPower(rightOutput);
+        rmotor2.setPower(rightOutput);
     }
 
 }
+
