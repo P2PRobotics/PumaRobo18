9d46e4827d0e41e2655580dc465ddca5451477aa
pumarobotics <p2probotics@gmail.com>
2019-01-05 14:07:05 -0700

    Removed state enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCraterSideOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCraterSideOp.java
@@ -2,28 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-enum State {
-    //keep track of states
-    INIT_DROP,
-    DROPPING,
-    INIT_UNLATCH,
-    UNLATCHING,
-    INIT_DRIVE,
-    DRIVE,
-    INIT_DEPOSIT,
-    DEPOSITING,
-    STOP
-
-//    SCAN,
-//    GOLD_LEFT,
-//    GOLD_CENTER,
-//    GOLD_RIGHT,
-//    DRIVE_DEPOT,
-//    DRIVE_CRATER,
-//    PLACE_MARKER,
-//    PARK
-}
-
 /**
  * ON CRATER SIDE:
  * hit element near crater, turn left to depot, place marker, park in crater if time permits
